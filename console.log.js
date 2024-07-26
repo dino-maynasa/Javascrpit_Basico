a1b5242b8278cf895fa081a3710149c688cbe451
Dino <dino.maynasa.maydana@gmail.com>
2024-07-26 09:02:21 -0400

    console.log

---
+++ console.log.js
@@ -0,0 +1,22 @@
+//console.log(); es una funcion integrada a JS que se utiliza para poder imprimir mensajes en consola del navegador o el editor de codigo
+console.log('Hola, JavaScript')
+// -> 'Hola, JavaScript'
+
+//concatenando
+const nombre = 'JavaScript'
+console.log('Hola, ' + nombre)
+// -> 'Hola, JavaScript'
+
+//mas metodos de console
+console.warn(); // imprime un mensaje de advertencia en la consola
+console.error(); //imprime un mensaje de error en la consola
+console.info(); // imprime un mensaje de información en la consola
+
+//ejemplos
+console.error('Error')
+// ❌ Error
+console.warn('Advertencia')
+// ⚠️ Advertencia
+console.info('Información')
+// ℹ️ Información
+
