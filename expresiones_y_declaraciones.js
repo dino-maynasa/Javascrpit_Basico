2c55eb5127854b9ededc68aa3f53e91d157c2c65
Dino <dino.maynasa.maydana@gmail.com>
2024-07-26 10:22:30 -0400

    expresiones y declaraciones

---
+++ expresiones_y_declaraciones.js
@@ -0,0 +1,10 @@
+//declaraciones  nop generan un valor, por lo tanto nos devuelve undefined, no produce un resultado
+let nombre = 'Daniel';
+
+
+//las expresiones son sentencias que nos devuelve un valor
+alert(2+2);
+/*
+if (expresion ){
+
+}*/
