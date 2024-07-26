557bd510b58cc46d3a1f343b15e4878d9eaea5ca
Dino <dino.maynasa.maydana@gmail.com>
2024-07-26 08:11:52 -0400

    operadores de comparacion

---
+++ operadores_comparacion.js
@@ -0,0 +1,28 @@
+//COMPARACION DE VALOPRES
+5 > 3 // true
+5 < 3 // false
+
+//OTRO TIPO DE COMPARADORES 
+5 >= 3 // true
+5 >= 5 // true
+5 <= 3 // false
+5 <= 5 // true
+
+//operadores de comparacion 
+5 === 5 // true
+5 !== 5 // false
+
+//COMPARANDO LAS CADENAS DE TEXTO*
+'JavaScript' === 'JavaScript' // true
+'JavaScript' === 'Java' // false
+"JavaScript" !== 'PHP' // true
+`Estoy Aprendiendo JavaScript` === 'Estoy Aprendiendo JavaScript' // true
+
+
+//COMPARACION DE BOOLEANOS*
+true > false // true
+false < true // true
+true > true // false
+false < false // false
+
+
