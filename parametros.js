050105d033347d88b11c6a304967f2e5cc6ae00b
Dino <dino.maynasa.maydana@gmail.com>
2024-07-26 11:09:02 -0400

    parametros y argumentos

---
+++ parametros.js
@@ -0,0 +1,17 @@
+// diferencias entre parametros y argumentos 
+
+//parametros
+/*function NombreParametro( parametro) {
+    return ;
+}*/
+
+//arguemntos
+
+/*
+function NombreParametro(argumento) {
+    return ;
+}
+    */
+
+
+//los orden de los parametros si importa r
