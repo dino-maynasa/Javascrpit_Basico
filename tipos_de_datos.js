784d20188afa3e8665cdeea4193d30772b4070c5
Dino <dino.maynasa.maydana@gmail.com>
2024-07-26 08:04:44 -0400

    tipos de datos

---
+++ tipos_de_datos.js
@@ -0,0 +1,37 @@
+//existen nueve tipos de datos diferentes, se divide en dos grupo:
+/* Primitivos: 
+numeros, cadenas de textos, booleanos, null, undfine¡d, bigint, symbol
+objetos en las funciones:
+
+
+ * OPERADORES ARITMETICOS*:
+-suma +
+-resta -
+-multiplicación  *
+-division /
+-modulo % (resto de la division)
+-exponente **
+
+*EJEMPLOS*
+2 + 2 // 4
+4 - 2 // 2
+3 * 2 // 6
+2 / 2 // 1
+2 % 2 // 0
+3 ** 3 // 27
+
+
+ *CADENAS DE TEXTO*
+'Estás aprendiendo JavaScript'
+"JavaScript te va a gustar"
+`Esto es una cadena de texto`
+
+*  CONCATENACIÓN*
+` texto  {suma()}`
+' texto'+ sum()
+"texto"+ sum()
+
+*BOOLEANOS*
+-true
+-false
+*/
