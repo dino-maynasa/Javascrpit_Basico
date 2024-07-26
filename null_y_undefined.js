9b39bb3a47e070cf05b43f0f418511c44102acfa
Dino <dino.maynasa.maydana@gmail.com>
2024-07-26 08:45:06 -0400

    null y undefined

---
+++ null_y_undefined.js
@@ -0,0 +1,11 @@
+// diferencias entre null y undefined 
+let valor; // una forma de poder declarar
+let valor1 = undefined; //otra forma de poder declarar undifined
+
+//comparaciones con null y undefined 
+null === undefined  // falso
+// solo cuanpo podemos comparar null con null obtenemos true 
+null === null; //true 
+
+//solo cuando comparamos undefined y undefined  se obtiene true 
+undefined === undefined; //true
