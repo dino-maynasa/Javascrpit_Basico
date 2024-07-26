77e797e6ca9d80b9eed6dc0665c73850c9b81084
Dino <dino.maynasa.maydana@gmail.com>
2024-07-26 08:36:59 -0400

    variables

---
+++ variables.js
@@ -0,0 +1,15 @@
+// se usa la palabra reservada let o const
+let number = 6;
+number += 2;
+number+1
+number ++;
+number -=2;
+
+
+let valorbooleanos = true;
+valorbooleanos =  false;
+
+
+//CONSTANTES 
+const PI = 3.1415
+//no se le puede reasiganar otro valor a una constante, por lo tanto siempre debe de ser inicializado con algun valor
