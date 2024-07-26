986267312ee0302784e68f2978760c7bd2fd30ba
Dino <dino.maynasa.maydana@gmail.com>
2024-07-26 08:24:01 -0400

    operadores logicos

---
+++ operadores_logicos.js
@@ -0,0 +1,18 @@
+//OPERADORES LOGICOS
+
+//operadores or ||
+console.log(true || true); // true (verdadero)
+console.log(false || true); // true
+console.log(true || false); // true
+console.log(false || false); // false (falso)
+
+//operador and &&
+lert(true && true); // true
+console.log(false && true); // false
+console.log(true && false); // false
+console.log(false && false); // false
+
+//operador not !== 
+console.log(!true); // false
+console.log(!0); // true
+
